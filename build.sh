c1fa9772be7f77d93715bc11d60c9113f10da99a
hylinktree@home <hylinktree@home.com>
2021-06-28 09:51:40 +0800

    nocomment

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GITBASE=iscr.io:5002
-docker build --no-cache . -t $GITBASE/iii/npmon
-#docker tag $GITBASE/npmon hylinktree/npmon
+TARGET=iii/nats-test
+
+docker build --no-cache . -t $GITBASE/$TARGET
 
