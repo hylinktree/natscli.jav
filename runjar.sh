fdbaae40d0da86f9ed2c78436c74415040206497
hylinktree@home <hylinktree@home.com>
2021-06-29 14:08:43 +0800

    nocomment

--- runjar.sh
+++ runjar.sh
@@ -1,1 +1,10 @@
-java -cp target/natscli-0.0.1-SNAPSHOT.jar -Dloader.main=io.nats.examples.jetstream.NatsJsPubAsync org.springframework.boot.loader.PropertiesLauncher -help
+if [[ $# < 2 ]]; then
+    echo usage:
+    echo java -cp target/natscli-0.0.1-SNAPSHOT.jar -Dloader.main=io.nats.examples.jetstream.NatsJsPubAsync org.springframework.boot.loader.PropertiesLauncher -help
+    exit
+fi
+jar=$1
+shift
+entry=$1
+shift
+java -cp $jar -Dloader.main=$entry org.springframework.boot.loader.PropertiesLauncher $*
