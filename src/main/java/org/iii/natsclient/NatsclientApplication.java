5839188bde96ca4517e087c8243f921077c71a01
hylinktree@home <hylinktree@home.com>
2021-06-29 13:41:44 +0800

    nocomment

--- src/main/java/org/iii/natsclient/NatsclientApplication.java
+++ src/main/java/org/iii/natsclient/NatsclientApplication.java
@@ -1,6 +1,8 @@
 package org.iii.natsclient;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.iii.natsclient.service.dummy;
 import org.springframework.boot.SpringApplication;
@@ -13,25 +15,57 @@ import io.nats.examples.autobench.NatsAutoBench;
 import io.nats.examples.autobench.mybench;
 import io.nats.examples.jetstream.NatsJsPub;
 
+final class Dispatcher {
+
+	private static final Map<String, Class<?>> ENTRY_POINTS = new HashMap<String, Class<?>>();
+	static {
+		ENTRY_POINTS.put("NatsAutoBench", NatsAutoBench.class);
+		// ENTRY_POINTS.put("bar", Bar.class);
+		// ENTRY_POINTS.put("baz", Baz.class);
+	}
+
+	public static void main(final String[] args) // throws Exception{
+	{
+
+		if (args.length < 1) {
+			// throw exception, not enough args
+		}
+		final Class<?> entryPoint = ENTRY_POINTS.get(args[0]);
+		if (entryPoint == null) {
+			return;
+			// throw exception, entry point doesn't exist
+		}
+		final String[] argsCopy = args.length > 1 ? Arrays.copyOfRange(args, 1, args.length) : new String[0];
+		try {
+			entryPoint.getMethod("main", String[].class).invoke(null, (Object) argsCopy);
+		} catch (Exception ex) {
+
+		}
+
+	}
+}
+
 @SpringBootApplication
 public class NatsclientApplication {
 
 	public static void main(String[] args) {
-		int len;
-		if ((len = args.length) < 1) {
-			ExampleUtils.Trace("Usage");
-			return;
-		}
-		String mod = args[0];
-		args = Arrays.copyOfRange(args, 1, len);
-		switch (mod) {
-			case "NatsAutoBench":
-				NatsAutoBench.main(args);
-				break;
-			default:
-				ExampleUtils.Trace("No target to run");
-				break;
-		}
+		Dispatcher.main(args);
+		// int len;
+		// if ((len = args.length) < 1) {
+		// ExampleUtils.Trace("Usage");
+		// return;
+		// }
+		// String mod = args[0];
+		// args = Arrays.copyOfRange(args, 1, len);
+		// switch (mod) {
+		// case "NatsAutoBench":
+		// NatsAutoBench.main(args);
+		// break;
+		// default:
+		// ExampleUtils.Trace("No target to run");
+		// break;
+		// }
+
 		// String[] ar = "-s hpcargo:4222 yysub 10".split(" ");
 		// NatsReply.main(ar);
 		// NatsJsPub.main(args);
